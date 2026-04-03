d61354ebc0e83ffd735fbeed29ad25af741d00ba
Alina Snäkin <alina.snakin@oppi.luovi.fi>
2026-04-03 11:00:23 +0300

    fix XSS via missing quote escaping in escapeHtml

--- src/js/utils.js
+++ src/js/utils.js
@@ -7,6 +7,8 @@ function toPosixPath(filePath) {
 function escapeHtml(text) {
   return text
     .replace(/&/g, "&amp;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;");
 }
